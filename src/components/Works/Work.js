02374ac5fccb790e0561774177232efdeaed852a
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2022-07-05 16:40:35 -0300

    Update Work.js

--- src/components/Works/Work.js
+++ src/components/Works/Work.js
@@ -33,8 +33,8 @@ const Work = ({work, setCurrent, id, currentId}) => {
               <p className="workType">{work.type}</p>
               <p className="workDescription">{work.description}</p>
               {lang === "esp"
-                ?<a className="workLink" href={`${work.web}`} target="_blank" rel="noreferrer">Visita el Proyecto +</a>
-                :<a className="workLink" href={`${work.web}`} target="_blank" rel="noreferrer">See the Project +</a>
+                ?<a className="workLink" href={`${work.web}`} target="_blank" rel="noreferrer">+ Visita el Proyecto</a>
+                :<a className="workLink" href={`${work.web}`} target="_blank" rel="noreferrer">+ See the Project</a>
               }              
             </div>
           </>
