ccf8eb599a5b6fc3c3e0c52cecaddc30b8d3ec1e
Eneas Baroni <eneasbaroni@gmail.com>
2022-07-09 11:17:30 -0300

    about V02

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -38,32 +38,32 @@ const About = () => {
         {lang === "esp"
             ?
               <>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">ARQUITECTO</h2>
-                  <p>Soy Arquitecto, recibido de la UNC. Tengo pasión por la arquitectura minimalista y brutalista. Un gran amor al hormigon y el metal</p>        
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">ARQUITECTO</h2>                  
+                  <p className="col-12 col-md-6 text-end">Soy Arquitecto, recibido de la UNC. Tengo pasión por la arquitectura minimalista y brutalista. Un gran amor al hormigon y el metal</p>        
                 </div>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">DISEÑADOR</h2>
-                  <p>Desde chico fui un curioso del arte y poder llevar eso hacia el diseño fue el camino que marco mi vida. En los años de facultad hice diferentes cursos de diseño gráfico que me permitieron adentrarme en la resolucion de problemas mediante el diseño</p>                       
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">DISEÑADOR</h2>                 
+                  <p className="col-12 col-md-6 text-end">Desde chico fui un curioso del arte y poder llevar eso hacia el diseño fue el camino que marco mi vida. En los años de facultad hice diferentes cursos de diseño gráfico que me permitieron adentrarme en la resolucion de problemas mediante el diseño</p>                       
                 </div>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">DESARROLLADOR</h2>
-                  <p>De la union del arte, diseño y la capacidad de poder crear cosas desde cero es que nace esta nueva faceta como desarrolador web. Donde me permito conjugar todas las experiencias y aptitudes adquiridas</p>
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">DESARROLLADOR</h2>                  
+                  <p className="col-12 col-md-6 text-end">De la union del arte, diseño y la capacidad de poder crear cosas desde cero es que nace esta nueva faceta como desarrolador web. Donde me permito conjugar todas las experiencias y aptitudes adquiridas</p>
                 </div>
               </>
             :
               <>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">ARCHITECT</h2>
-                  <p>I am an architect, graduated from UNC. I have a passion for minimalist and brutalist architecture. A strong love for concrete and metal</p>        
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">ARCHITECT</h2>
+                  <p className="col-12 col-md-6 text-end">I am an architect, graduated from UNC. I have a passion for minimalist and brutalist architecture. A strong love for concrete and metal</p>        
                 </div>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">DESIGNER</h2>
-                  <p>Since I was child, I have curious about art and being able to mix that with design was a way of life. In University I studied graphic design, that allowed me to go deep into the problem solving with design</p>                       
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">DESIGNER</h2>
+                  <p className="col-12 col-md-6 text-end">Since I was child, I have curious about art and being able to mix that with design was a way of life. In University I studied graphic design, that allowed me to go deep into the problem solving with design</p>                       
                 </div>
-                <div className="col-12 col-md-4 text-end text-md-start row aboutText justify-content-center mt-3 mt-md-0">
-                  <h2 className="h1">DEVELOPER</h2>
-                  <p>From the union of art, design and the ability to create things from scratch was born this new facet as a web developer. Where I let myself combine all the experiences and skills acquired</p>
+                <div className="col-12 row text-end text-md-start row aboutText my-1">
+                  <h2 className="h1 col-12 col-md-6">DEVELOPER</h2>
+                  <p className="col-12 col-md-6 text-end">From the union of art, design and the ability to create things from scratch was born this new facet as a web developer. Where I let myself combine all the experiences and skills acquired</p>
                 </div>
               </>
           }
