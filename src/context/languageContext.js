55ce358365d40e0de9fc989d1471ace82b52e249
Eneas Baroni <eneasbaroni@gmail.com>
2022-10-03 20:34:10 -0300

    Languages

--- src/context/languageContext.js
+++ src/context/languageContext.js
@@ -4,7 +4,11 @@ const LanguageContext = createContext()
 
 const LanguageProvider = ({children}) => {
   const [language, setLanguage] = useState("esp")
-  const handleLanguage = (lg) => setLanguage(lg);
+  const handleLanguage = (lg) =>{ 
+    setLanguage(lg);
+    lg === "esp" && document.documentElement.setAttribute('lang', 'es')
+    lg === "eng" && document.documentElement.setAttribute('lang', 'en')
+  }
 
   const data = {language, handleLanguage}
   return <LanguageContext.Provider value={data}> {children} </LanguageContext.Provider>
