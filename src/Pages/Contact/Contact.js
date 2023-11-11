403b064f34334639b8b80e788e9a4c522a8b34cd
Eneas <87401725+eneasbaroni@users.noreply.github.com>
2023-11-11 18:20:59 -0300

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -46,9 +46,10 @@ const Contact = () => {
               <p className='mainText'>or connect with me on socials</p>
           }
           
+          <div><a className="mediaLink h1" href="https://www.linkedin.com/in/eneasbaroni" target="_blank" rel="noreferrer">LINKEDIN</a></div>
+          <div><a href="https://wa.me/+5493512110423" target="_blank" rel="noreferrer">WHATSAPP</a></div>
           <div><a className="mediaLink h1" href="https://www.instagram.com/mr_eneas/" target="_blank" rel="noreferrer">INSTAGRAM</a></div>
           <div><a className="mediaLink h1" href="https://github.com/eneasbaroni" target="_blank" rel="noreferrer">GITHUB</a></div>
-          <div><a className="mediaLink h1" href="https://www.linkedin.com/in/eneasbaroni" target="_blank" rel="noreferrer">LINKEDIN</a></div>
           {lang === "esp"
             ?
               <a className="cv" href="/docs/CV_BARONI_Eneas.pdf" target="_blank" rel="noreferrer" download>
